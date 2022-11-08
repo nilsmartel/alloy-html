bd09e9c3716e0f6f6d5ae3165879b7d0c3aeb61d
Nils Martel <nilsmartel@yahoo.de>
2022-11-08 14:44:18 +0100

    Set up parser trait

--- alloy-parser/src/lib.rs
+++ alloy-parser/src/lib.rs
@@ -1,16 +1,47 @@
 pub struct Node {
-    kind: String,
+    kind: Ident,
     idsAndClasses: Vec<IdOrClass>,
     attributes: Vec<Attribute>,
     body: Vec<Node>,
 }
 
 pub enum IdOrClass {
-    Id(String),
-    Class(String),
+    Id(Ident),
+    Class(Ident),
 }
 
 pub struct Attribute {
-    key: String,
+    key: Ident,
     value: String,
+}
+
+pub struct Ident(String);
+
+pub trait Parser
+where
+    Self: Sized,
+{
+    fn parse(input: &str) -> nom::IResult<&str, Self>;
+
+    fn parse_trim(input: &str) -> nom::IResult<&str, Self> {
+        let input = input.trim_start();
+
+        // cut out commments
+        if input.starts_with("//") {
+            if let Some(index) = input.find('\n') {
+                return Self::parse_trim(&input[index..]);
+            }
+            return Self::parse_trim("");
+        }
+        /* cut out comments */
+        if input.starts_with("/*") {
+            if let Some(index) = input.find("*/") {
+                return Self::parse_trim(&input[index..]);
+            }
+            // It's allowed to simply cut off all remaining content without closing */
+            return Self::parse_trim("");
+        }
+
+        Self::parse(input)
+    }
 }
